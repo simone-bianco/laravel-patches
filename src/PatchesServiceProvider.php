1845285e8ff94f3fcc00b6cb1157bcf8d99a9d09
simone-bianco <simone9292@live.com>
2025-09-25 18:38:19 +0200

    + first commit

--- src/PatchesServiceProvider.php
+++ src/PatchesServiceProvider.php
@@ -2,15 +2,13 @@
 
 namespace SimoneBianco\Patches;
 
-// Import necessari dal package Spatie e i tuoi comandi
+use SimoneBianco\Patches\Console\Commands\RollbackDataPatch;
+use SimoneBianco\Patches\Console\Commands\RunSinglePatch;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use SimoneBianco\Patches\Console\Commands\ApplyDataPatches;
 use SimoneBianco\Patches\Console\Commands\MakePatchCommand;
-// Assumi che questi comandi esistano nel tuo namespace, altrimenti aggiorna il path
-use App\Console\Commands\RollbackDataPatch;
-use App\Console\Commands\RunSinglePatch;
 
 
 class PatchesServiceProvider extends PackageServiceProvider
@@ -23,15 +21,11 @@ class PatchesServiceProvider extends PackageServiceProvider
      */
     public function configurePackage(Package $package): void
     {
-        /*
-         * Questo metodo centrale definisce tutto ciò che il tuo pacchetto
-         * offre all'applicazione Laravel.
-         */
         $package
-            ->name('sb-patches') // Nome del pacchetto, usato per config, views, etc.
-            ->hasConfigFile()    // Registra e permette la pubblicazione del file di configurazione
-            ->hasMigrations(['create_patches_table', 'add_another_column_to_patches_table'])
-            ->hasCommands([      // Registra i comandi Artisan
+            ->name('laravel-patches')
+            ->hasConfigFile('sb-patches')
+            ->hasMigrations(['0001_01_01_000100_create_data_patches_table'])
+            ->hasCommands([
                 MakePatchCommand::class,
                 ApplyDataPatches::class,
                 RunSinglePatch::class,
